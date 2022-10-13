79fae82c0bda374637d5658632b872a0c27f1ec0
Ogwu Emmanuel berit <beritogwu@gmail.com>
2022-10-13 16:14:40 -0700

    6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 fprintf(stderr, "[Anything]");
 return (0);
 }

5ad841116843e35b3056dde11b806c3d3eed90bb
Ogwu Emmanuel berit <beritogwu@gmail.com>
2022-10-13 16:11:28 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char: %c byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int: %i byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %li byte(s)\n", (unsigned long)sizeof(li));
-printf("Size of a long long int: %lli byte(s)\n", (unsigned long)sizeof(lli));
-printf("Size of a float: f: %f byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("Size of a float: f: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }

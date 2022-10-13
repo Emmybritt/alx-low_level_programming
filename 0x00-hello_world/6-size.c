74f6363bd3cc2b9bea6e47ac6d39c30587a6e94a
Ogwu Emmanuel berit <beritogwu@gmail.com>
2022-10-13 16:12:50 -0700

    5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-printf("Size of a float: f: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }

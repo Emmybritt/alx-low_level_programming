01c22c5fe84d9b493f401f7e7b3f2ce30413aada
Ogwu Emmanuel berit <beritogwu@gmail.com>
2022-10-14 12:55:26 -0700

    modified 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-putchar((long)"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+putchar((2)"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59;
 return (1);
 }

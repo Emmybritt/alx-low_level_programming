1ae92e7179b3b847d46c0436aa9cf4e11ad61d56
Ogwu Emmanuel berit <beritogwu@gmail.com>
2022-10-15 11:18:23 -0700

    modified

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf(n"%d\n is positive");
+printf("%d is positive\n", n);
 else if (n == 0)
-printf(n"%d\n is zero");
+printf("%d is zero\n", n);
 else if (n < 0)
-printf(n"%d\n is negative");
+printf("%d is negative\n", n);
 return (0);
 }

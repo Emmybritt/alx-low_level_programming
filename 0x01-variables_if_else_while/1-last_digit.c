232e8ed420d8426ccd987ce3ba9711395e5a0480
Ogwu Emmanuel berit <beritogwu@gmail.com>
2022-10-15 12:18:14 -0700

    created 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 5)
+{
+printf("and is greater than 5");
+}
+else if (n == 0)
+{
+printf("and is 0");
+}
+else if (n < 6 && n != 0)
+{
+printf("and is less than 6 and not 0");
+}
+return (0);
+}

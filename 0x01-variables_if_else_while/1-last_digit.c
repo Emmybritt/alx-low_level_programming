0490d6240dd2e9f437969af72669556bba07a2f3
Ogwu Emmanuel berit <beritogwu@gmail.com>
2022-10-15 12:39:00 -0700

    modifuef

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("and is greater than 5");
+printf("Last digit of 98 is %d and is greater than 5", n);
 }
 else if (n == 0)
 {
-printf("and is 0");
+printf("Last digit of 980 is %d and is 0", n);
 }
 else if (n < 6 && n != 0)
 {
-printf("and is less than 6 and not 0");
+printf("Last digit of -980 is %d and is less than 6 and not 0", n);
 }
 return (0);
 }

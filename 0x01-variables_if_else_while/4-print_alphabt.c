e13e420a65f46f48fca6a1094637406fb356abe6
Ogwu Emmanuel berit <beritogwu@gmail.com>
2022-10-16 05:26:10 -0700

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch != 'q' || ch != 'e')
+{
+putchar(ch);
+}
+ch++;
+}
+putchar('\n');
+return (0);
+}

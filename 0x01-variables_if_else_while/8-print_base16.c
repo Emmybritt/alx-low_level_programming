a4958e10ec60b6225cebeafd1f3a34e631bbaca1
Ogwu Emmanuel berit <beritogwu@gmail.com>
2022-10-17 06:00:04 -0700

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = '0';
+while (ch <= 15)
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
